962369ecc0ec3b18e9ef285d56bb5758a0184c59
Samuel <silalahisamueldaerren@gmail.com>
2024-04-25 18:16:54 +0700

    projek oop

--- oop1.php
+++ oop1.php
@@ -92,7 +92,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Pembelian Bahan Bakar</title>
-    <link rel="stylesheet" href="oop.css">
+    <link rel="stylesheet" href="oop1.css">
 </head>
 <body>
 <div class="form_pembelian">
